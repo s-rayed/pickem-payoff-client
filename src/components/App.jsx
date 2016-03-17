9b65fa1b2dc0d02b1fdd96b9dab70222ec3ee8cc
Jonathan Salis <jon@functionalimperative.com>
2016-03-17 15:51:32 -0400

    Adding App.jsx

---
+++ src/components/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    }
+  };
+
+  handleClick = () => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        This is my app!!!! { this.state.counter }
+        <br />
+        <button onClick={this.handleClick}>Click me!!</button>
+      </div>
+    )
+  }
+  
+}
+
+export default App;
